2c5a547c011546c9c8aa8ec1d17650aa8fd6400e
Andris Raugulis <araugulis@evolutiongaming.com>
2014-08-21 18:40:56 +0300

    Fix duplicated rule detection and exclude single such rule.

--- find.duplicated_rules.sh
+++ find.duplicated_rules.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cd $(dirname $0)
-grep '<rule key=' rules-*.xml | cut -d '"' -f 2 | sort | uniq -c | sort -n
+_outdir='build'
+cd $(dirname $0) && cd ${_outdir}
+grep '<rule key=' rules*.xml | cut -d '"' -f 2 | sort | uniq -c | sort -n | awk '{ print $1 " " $2 }' | grep -v "^1 "
