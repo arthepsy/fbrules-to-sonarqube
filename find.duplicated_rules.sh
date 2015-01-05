0a4b18aece308e54a9090245aff8c9e1124cc8d2
Andris Raugulis <araugulis@evolutiongaming.com>
2015-01-05 19:01:15 +0200

    Fix also duplication finder script.

--- find.duplicated_rules.sh
+++ find.duplicated_rules.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 _outdir='build'
-cd $(dirname $0) && cd ${_outdir}
+_cdir=$(cd -- "$(dirname "$0")" && pwd)
+cd -- "${_cdir}/${_outdir}"
 grep '<rule key=' rules*.xml | cut -d '"' -f 2 | sort | uniq -c | sort -n | awk '{ print $1 " " $2 }' | grep -v "^1 "
