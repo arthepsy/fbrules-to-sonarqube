39fe52dbedd6c360da595230be970e34b053bfd9
Andris Raugulis <araugulis@evolutiongaming.com>
2015-02-24 16:19:47 +0200

    Deploy html files only if they are present.

--- deploy.sh
+++ deploy.sh
@@ -22,6 +22,8 @@ if [ -d "${_odir}" ]; then
 	find "${_odir}" -name "rules*.xml" -exec cp "{}" "${_dir1}/" \;
 	find "${_odir}" -name "profile-findbugs.xml" -exec cp "{}" "${_dir1}/" \;
 	find "${_odir}" -name "findbugs.properties" -exec cp "{}" "${_dir2}/" \;
-	find "${_odir}/html/findbugs/" -name "*.html" -exec cp "{}" "${_dir3}/" \;
+	if [ -d "${_odir}/home/findbugs" ]; then
+		find "${_odir}/html/findbugs/" -name "*.html" -exec cp "{}" "${_dir3}/" \;
+	fi
 fi
 
